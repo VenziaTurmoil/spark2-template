0f3aa1baf8195df83cd5c6ec793e3d50440898f1
Damien D'ARRAS <damien.d-arras@ensg.eu>
2023-01-26 15:54:57 +0100

    ex4

--- src/main/scala/sql_practice/examples.scala
+++ src/main/scala/sql_practice/examples.scala
@@ -102,10 +102,95 @@ object examples {
         sample7.col("total_emp").as("emp7"),
         sample8.col("total_emp").as("emp8"),
         (sample8.col("total_emp")-sample7.col("total_emp")).as("job_dif"))
-      .filter(sample7.col("salary") > 100000
+      .filter((sample7.col("salary") > 100000
         || sample8.col("salary") > 100000)
+        && $"job_dif" <= 0)
       .orderBy("job_dif")
 
     job_loss.show()
+  }
+
+  def exec4(): Unit = {
+    val spark = SessionBuilder.buildSession()
+    import spark.implicits._
+
+    val toursDF = spark.read
+      .option("multiline", true)
+      .option("mode", "PERMISSIVE")
+      .json("data/input/tours.json")
+
+    val unique_difficulties = toursDF
+      .select("tourDifficulty")
+      .distinct()
+      .count()
+
+    print("Number of unique Difficulties : " + unique_difficulties + "\n")
+
+    val stats_prices = toursDF
+      .agg(min("tourPrice").as("min_price"),
+        max("tourPrice").as("max_price"),
+        avg("tourPrice").as("avg_price"))
+
+    stats_prices.show()
+
+    val stats_price_difficulties = toursDF
+      .groupBy("tourDifficulty")
+      .agg(min("tourPrice").as("min_price"),
+        max("tourPrice").as("max_price"),
+        avg("tourPrice").as("avg_price"))
+
+    stats_price_difficulties.show()
+
+    val stats_price_length_difficulties = toursDF
+      .groupBy("tourDifficulty")
+      .agg(min("tourPrice").as("min_price"),
+        max("tourPrice").as("max_price"),
+        avg("tourPrice").as("avg_price"),
+        min("tourLength").as("min_length"),
+        max("tourLength").as("max_length"),
+        avg("tourLength").as("avg_length"))
+
+    stats_price_length_difficulties.show()
+
+    val tags10 = toursDF
+      .select(explode($"tourTags").as("tag"))
+      .groupBy("tag")
+      .count().as("count")
+      .orderBy($"count".desc)
+      .limit(10)
+
+    tags10.show()
+
+    val tags_difficulty = toursDF
+      .select(explode($"tourTags").as("tag"), $"tourDifficulty")
+      .groupBy("tag", "tourDifficulty")
+      .count().as("count")
+      .orderBy($"count".desc)
+
+    val tag10_difficulty = tags10
+      .join(tags_difficulty,
+        tags10.col("tag") === tags_difficulty.col("tag"),
+        "left")
+      .select(
+        tags10.col("tag"),
+        $"tourDifficulty",
+        tags10.col("count").as("total"),
+        tags_difficulty.col("count").as("count"))
+      .orderBy($"total".desc, $"tag", $"count".desc)
+
+    tag10_difficulty.show()
+
+    val stats_tags = toursDF
+      .select(
+        explode($"tourTags").as("tag"),
+        $"tourDifficulty",
+        $"tourPrice")
+      .groupBy("tag", "tourDifficulty")
+      .agg(min("tourPrice").as("min_price"),
+        max("tourPrice").as("max_price"),
+        avg("tourPrice").as("avg_price"))
+      .orderBy($"avg_price".desc)
+
+    stats_tags.show()
   }
 }
